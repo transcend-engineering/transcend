a4391012b71fab5a2d29b6ca046391375e0a980a
Jake Dexheimer <jakedex@gmail.com>
2016-02-29 02:09:09 -0600

    Start voting

---
+++ vote/js/vote.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  setInterval(function () {
+    $.get("http://tm-vote.herokuapp.com/list", function (data) {
+      
+      for (var i = 0; i < data.projects.length; i++) {
+        var project = data.projects[i];
+        $("#project" + i + " .votes").html(project.votes);
+        $("#project" + i + " .bar").css("width", project.votes * 3.5)
+      }
+      console.log('Load was performed.');
+    });
+  }, 2000);
+});
