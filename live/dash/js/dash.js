47b8b6db15b63cd85d394ccba1b73db220b0583d
Jake Dexheimer <jakedex@gmail.com>
2016-03-07 22:50:36 -0600

    Christian backend to ul>li eles

--- live/dash/js/dash.js
+++ live/dash/js/dash.js
@@ -10,4 +10,25 @@ $(function() {
       $hero_desc.text(data.msg);
     }
   });
+  
+  var $annc_list = $('<ul class="list-group"></ul>');
+  var $list_location = $('.ajax-list');
+  
+  console.log("attempting ajax 2");
+  $.ajax({
+    type: 'GET',
+    url: 'http://cheerchuck.me/transcend/index.php',
+    dataType    :   'json',
+    success: function(data) {
+      console.log(data);
+      for(var i=0; i < data.length; i++){ 
+        var item = data[i]; 
+        console.log(item);
+        $annc_list.append($('<li class="list-group-item"></li>').text(item));
+      }
+      
+      console.log($annc_list);
+      $annc_list.appendTo($list_location);
+    }
+  });
 });
