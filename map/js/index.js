98940ac2aa1ac485a314129f291daa3e08d78454
Michael Fix <mfix@wisc.edu>
2016-05-26 00:12:10 -0500

    fix legend to have greyscale and toggling

--- map/js/index.js
+++ map/js/index.js
@@ -8,14 +8,25 @@ var filter;
 
 $('.legend-img').click(function(){
 	// console.log(this.getAttribute('data-select'));
-	filterMap(this.getAttribute('data-select'));
+	filterMap(this);
 });
 
+var bools = [false, false, false, false, false];
 function filterMap (filterNum) {
-	switch(filterNum){
-		case '1':
+	var n = parseInt(filterNum.getAttribute('data-select'));
+	if (bools[n-1]){
+		bools = [false, false, false, false, false];
+		n = 0;
+	}
+	else{
+		bools[n-1] = true;
+		$('.legend-img').addClass('greyscale');
+		$(filterNum).removeClass('greyscale');
+	}
+
+	switch(n){
+		case 1:
 			var filter = MLayer.setFilter(function(f){
-				console.log('here');
 				return (f.properties['marker-color'] === '#004400') ||
 					   (f.properties['marker-color'] === '#006600') ||
 					   (f.properties['marker-color'] === '#009900') ||
@@ -23,31 +34,32 @@ function filterMap (filterNum) {
 			});
 			return false;
 			break;
-		case '2':
+		case 2:
 			var filter = MLayer.setFilter(function(f){
 				return (f.properties['marker-color'] === '#0511b6');
 			});
 			return false;
 			break;
-		case '3':
+		case 3:
 			var filter = MLayer.setFilter(function(f){
 				return f.properties['marker-color'] === '#6f00ae';
 			});
 			return false;
 			break;
-		case '4':
+		case 4:
 			var filter = MLayer.setFilter(function(f){
 				return f.properties['marker-color'] === '#fdff16';
 			});
 			return false;
 			break;
-		case '5':
+		case 5:
 			var filter = MLayer.setFilter(function(f){
 				return f.properties['marker-color'] === '#fb6d14';
 			});
 			return false;
 			break;
 		default:
+			$('.legend-img').removeClass('greyscale');
 			var filter = MLayer.setFilter(function(f){
 				return true;
 			});
