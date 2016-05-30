a3d41a572a349235b8f4b22a3032f651f86578ee
Michael Fix <mfix@wisc.edu>
2016-05-30 15:08:14 -0700

    clustering works, but don't use

--- map/js/index.js
+++ map/js/index.js
@@ -1,28 +1,51 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoibWZpeDIyIiwiYSI6IjZmNDIwZGZkOWI1ZDgwY2VkNGRkOTVjNjcwMTMxMDEyIn0.QnJDKEfnvhw8u0zVtWVRiw';
-        var map = L.mapbox.map('map', 'mapbox.streets')
-            .setView([43.080788, -89.406], 12);	
-var MLayer = L.mapbox.featureLayer(DATA).addTo(map);
+        var map = L.mapbox.map('map')
+            .setView([43.080788, -89.406], 12)
+            .addLayer(L.mapbox.tileLayer('mapbox.streets'));	
+var MLayer = L.mapbox.featureLayer(DATA);
+MLayer.addTo(map);
 var client = L.mapbox.geocoderControl('mapbox.places');
 map.addControl(client);
 var filter;
 
+
+// var markers = new L.MarkerClusterGroup();
+
+// for (var i = 0; i < DATA.length; i++) {
+// 	console.log("1");
+//     var lat = parseFloat(DATA[i].geometry.coordinates[1]);
+// 	var lon = parseFloat(DATA[i].geometry.coordinates[0]);
+
+//     var title = DATA[i].properties.title;
+//     var marker = L.marker(new L.LatLng(lat, lon), {
+//         icon: L.mapbox.marker.icon({'marker-symbol': 'post', 'marker-color': '0044FF'}),
+//         title: title
+//     });
+//     marker.bindPopup(title);
+//     markers.addLayer(marker);
+// }
+
+// map.addLayer(markers);
+
+
 $('.legend-img').click(function(){
-	// console.log(this.getAttribute('data-select'));
 	filterMap(this);
 });
 
 var bools = [false, false, false, false, false];
 function filterMap (filterNum) {
 	var n = parseInt(filterNum.getAttribute('data-select'));
-	if (bools[n-1]){
+	if (n == 0 || bools[n-1]){
 		bools = [false, false, false, false, false];
 		n = 0;
 	}
 	else{
+		bools = [false, false, false, false, false];
 		bools[n-1] = true;
 		$('.legend-img').addClass('greyscale');
 		$(filterNum).removeClass('greyscale');
 	}
+	
 
 	switch(n){
 		case 1:
