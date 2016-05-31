3c9d523fb1dcdf0220f92a2d74bf74ce34bb3846
Michael Fix <mfix@wisc.edu>
2016-05-30 19:29:03 -0700

    start Stripe checkout in /map

---
+++ map/js/stripe.js
@@ -0,0 +1,26 @@
+var handler = StripeCheckout.configure({
+    key: 'pk_live_yijbiChHJfBMpi5iWVaibAf1',
+    image: '/img/transcend_square.png',
+    locale: 'auto',
+    token: function(token) {
+      console.log(token);
+      console.log(token.id);
+      // You can access the token ID with `token.id`.
+      // Get the token ID to your server-side code for use.
+    }
+  });
+
+$('#stripe_button').on('click', function(e) {
+  // Open Checkout with further options:
+  handler.open({
+    name: 'Entrepreneurship EQ',
+    description: '1 Spot on the Map',
+    amount: 1500
+  });
+  e.preventDefault();
+});
+
+// Close Checkout on page navigation:
+$(window).on('popstate', function() {
+  handler.close();
+});
